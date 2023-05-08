eac9f9ac056a6953cff9f4781ad8a9c6a253a790
chriford <siamechrif@gmail.com>
2023-05-08 14:55:24 +0200

    design payment status templates

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,14 @@
-"""core URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from os import name
 from django.contrib import admin
 from django.urls import path
-from django_project.views import index
+from django.conf import settings
+from django.conf.urls.static import static
+from django_project.views import index, payment_response
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name="index"),
-]
+    path('callback', payment_response, name='payment_response')
+ ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django_project/views.py
+++ django_project/views.py
@@ -1,26 +1,11 @@
+import os
+import random
+import math
+import requests
 import environ
 from django.views.decorators.http import require_http_methods
 from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-
-# Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
-
-
-def index(request):
-    libraries = [0,2,2]
-    return render(request, 'index.html', {
-        'libraries': libraries,
-        'public_key': 'FLWPUBK_TEST-d1765bbd42307aad3ccb6fa9d59e21d9-X',
-        'private_key': 'FLWSECK_TEST-a987ec54fe7b47dfa301709a3a4096d5-X',
-        'encryption_key': 'FLWSECK_TEST1dc50b94be9c',
-    })
-
-
-
+from django.shortcuts import render, redirect
 
 @require_http_methods(['GET', 'POST'])
 def payment_response(request):
@@ -28,4 +13,73 @@ def payment_response(request):
     tx_ref=request.GET.get('tx_ref', None)
     print(status)
     print(tx_ref)
-    return HttpResponse('Finished')
+    return render(request, 'flutterwave/payment_callback.html', {
+        'payment_status': status,
+        'tx_ref': tx_ref,
+    })
+
+
+def process_payment(name,email,amount,phone):
+    auth_token= 'FLWSECK_TEST-a987ec54fe7b47dfa301709a3a4096d5-X'
+    hed = {'Authorization': 'Bearer ' + auth_token}
+    data = {
+        "tx_ref":''+str(math.floor(1000000 + random.random()*9000000)),
+        "amount":amount,
+        "currency":"ZMW",
+        "redirect_url": "http://localhost:8000/callback",
+        "payment_options":"card, mobilemoneyghana, barter",
+        "meta":{
+            "consumer_id":23,
+            "consumer_mac":"92a3-912ba-1192a"
+        },
+        "customer":{
+            "email":email,
+            "phonenumber":phone,
+            "name":name
+        },
+        "customizations":{
+            "title": "Membership Payment",
+            "description": "chase your dreams with the right tool -> Books",
+            "logo":"https://img.freepik.com/free-vector/gradient-library-logo-template_23-2149330624.jpg"
+        }
+    }
+    url = ' https://api.flutterwave.com/v3/payments'
+    response = requests.post(
+        url, 
+        json=data, 
+        headers=hed,
+    )
+    response=response.json()
+    link=response['data']['link']
+    return link
+    
+
+@require_http_methods(['GET', 'POST'])
+def index(request):
+    name=  'Patrick Patrick'
+    email = 'patrick@gmail.com'
+    amount = 150
+    phone = '260762412680'
+    if request.method=='POST':
+        return redirect(
+            str(
+                process_payment(
+                    name=name,
+                    email=email,
+                    amount=amount,
+                    phone=phone
+                )
+            )
+        )
+    libraries = [0,2,2]
+    return render(request, 'index.html', {
+        'libraries': libraries,
+        'customer': dict(
+            name = name,
+            email = email,
+            amount = amount,
+            phone = phone,    
+        ),
+        'public_key': 'FLWPUBK_TEST-d1765bbd42307aad3ccb6fa9d59e21d9-X',
+    })
+
