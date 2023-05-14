1d1e4de1740a1d04caa0962087dd5047a806064c
chriford <siamechrif@gmail.com>
2023-05-14 21:08:59 +0200

    create the hotel model, update files

--- django_project/admin.py
+++ django_project/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import (
+    Hotel
+)
+
+admin.site.register(Hotel)
 
 # Register your models here.
--- django_project/models.py
+++ django_project/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _ # noqa
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+class Hotel(models.Model):
+    image = models.ImageField(
+        upload_to="media/%H-%S-%M/",
+        null=True, 
+        blank=True,
+    )
+    name = models.CharField(max_length=200)
+    description = models.TextField(
+        validators=[MinLengthValidator(50), MaxLengthValidator(300)],
+        help_text=_("what's interesting about this hotel?"),
+        blank=False,
+        null=True,
+    )
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
 
-# Create your models here.
+    def __str__(self) -> str:
+        return self.name
